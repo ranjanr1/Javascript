7706f66d61298b5c831cb42507d0ef31e98c7382
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-03-05 16:37:26 -0500

    Test

--- basics-02-added-scripts-imports/assets/scripts/app.js
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,1 +1,6 @@
-alert('This works!');
+const currentResult = 5;
+let calculatondescription = `(${currentResult} + 10 \n) +10*3-1`;
+let errorMessage = 'This was the Test\n '+ 
+' I was trying something out' ;
+//outputResult(currentResult, errorMessage);
+alert(errorMessage);
--- basics-02-added-scripts-imports/assets/scripts/vendor.js
+++ basics-02-added-scripts-imports/assets/scripts/vendor.js
@@ -8,6 +8,7 @@ const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
 function outputResult(result, text) {
+  userInput.text = text;
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
