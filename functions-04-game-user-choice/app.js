eb001a9f3ec0ec911fae99c5c1ee27d132cf6a5e
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-03-08 15:17:25 -0500

    final

--- functions-04-game-user-choice/app.js
+++ functions-04-game-user-choice/app.js
@@ -3,18 +3,21 @@ let gameIsRunning =false;
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
+const RESULT_DRAW = 'Draw';
+const RESULT_CWIN = 'Computer';
+const RESULT_PWIN = 'Player';
 
-
-const getPlayerChoice = function() {
+const getPlayerChoice = ()=> {
     const selection = prompt(`${ROCK},${PAPER} or ${SCISSORS} ? `,'').toUpperCase();
     if(selection != ROCK && selection != PAPER 
       && selection != SCISSORS)
        alert(`Invalid Choice! We chose ${ROCK} for you!`); 
-      else
-      alert(`You chose ${selection} for yourself!`); 
+       else
+       return selection;
+     
 };
 
-const getComputerChoice = function() {
+const getComputerChoice = () =>{
       const valueRandom = Math.random();
       if(valueRandom <.34)
          return ROCK;      
@@ -24,10 +27,28 @@ const getComputerChoice = function() {
         return SCISSORS;      
 }
 
-startGameBtn.addEventListener('click', function() {
+const getWinner = (cChoice,pChoice)=> {
+  if(pChoice === cChoice)
+    return RESULT_DRAW;
+  else if(cChoice === ROCK && pChoice ===PAPER ||
+    cChoice===PAPER && cChoice ===SCISSORS ||
+    cChoice === SCISSORS && pChoice ===ROCK)
+    return RESULT_PWIN;
+  else
+    return RESULT_CWIN; 
+}
+
+const startGame = () =>{
   if(gameIsRunning)
-    return;
-  console.log('Game is starting...');
-  gameIsRunning = true;
-  const playerSelection = getPlayerChoice();
-});
+  return;
+console.log('Game is starting...');
+gameIsRunning = true;
+const playerSelection = getPlayerChoice();
+const computerselection = getComputerChoice();
+const winner = getWinner(playerSelection,computerselection);
+console.log(`Player Selected ${playerSelection}`);
+console.log(`Computer Selected ${computerselection}`);
+console.log(`Winner is ${winner}`);
+gameIsRunning = false;
+}
+startGameBtn.addEventListener('click', startGame);
