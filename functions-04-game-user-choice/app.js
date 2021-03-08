34f4a39d0433cbb953e9d671dde07f7f8ba8a540
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-03-08 14:01:45 -0500

    add and commit seleciton funtion

--- functions-04-game-user-choice/app.js
+++ functions-04-game-user-choice/app.js
@@ -1,8 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn'); 
-
+let gameIsRunning =false;
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
+
 
 const getPlayerChoice = function() {
     const selection = prompt(`${ROCK},${PAPER} or ${SCISSORS} ? `,'').toUpperCase();
@@ -13,7 +14,20 @@ const getPlayerChoice = function() {
       alert(`You chose ${selection} for yourself!`); 
 };
 
+const getComputerChoice = function() {
+      const valueRandom = Math.random();
+      if(valueRandom <.34)
+         return ROCK;      
+      else if( valueRandom>.34 && valueRandom <.67)
+        return PAPER;      
+      else 
+        return SCISSORS;      
+}
+
 startGameBtn.addEventListener('click', function() {
+  if(gameIsRunning)
+    return;
   console.log('Game is starting...');
+  gameIsRunning = true;
   const playerSelection = getPlayerChoice();
 });
